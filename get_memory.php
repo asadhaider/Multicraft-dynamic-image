e736eb134a6fcaf657b15efa0e7cab8c5e3cf68b
Asad Haider <asad@asadhaider.co.uk>
2012-10-27 00:32:48 +0100

    Dynamic memory usage image.
    Signed-off-by: Asad Haider <asad@asadhaider.co.uk>

--- get_memory.php
+++ get_memory.php
@@ -1,5 +1,15 @@
 <?php
+$png_image = './images/banner.png';
+$font_path = './fonts/bellerose-webfont.ttf';
+
 $total_memory = file_get_contents("./memory.cache");
 
-print("Total Memory Used: " . $total_memory);
+$im = imagecreatefrompng($png_image);
+$textcolor = imagecolorallocate($im, 255, 255, 255);
+
+imagestring($im, 5, 130, 12, "Total Amount Of Ram Online", $textcolor);
+imagestring($im, 5, 200, 32, $total_memory . " MB", $textcolor);
+
+header("Content-type: image/png");
+imagepng($im);
 ?>
