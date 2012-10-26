c14d57764b7e15c314d51a7bd9436cda789f9813
Asad Haider <asad@asadhaider.co.uk>
2012-10-26 23:35:44 +0100

    Get memory.
    Signed-off-by: Asad Haider <asad@asadhaider.co.uk>

--- memory.php
+++ memory.php
@@ -4,3 +4,20 @@ require('config/servers.php');
 
 header("Content-type: text/plain");
 
+$total_memory = 0;
+
+foreach( $servers as $server ) {
+	$api = new MulticraftAPI($server['API_URL'], $server['API_USER'], $server['API_PASS']);
+	$server_ids = $server['SERVER_IDS'];
+	
+	foreach( $server_ids as $server_id ) {
+		$memory_query = $api->getConnectionMemory($server_id, 0);
+		
+		if( $memory_query['success'] == 1 ) {
+			$total_memory += $memory_query['data']['used'];
+		}
+	}
+}
+
+print("Total Memory Used: " . $total_memory);
+?>
