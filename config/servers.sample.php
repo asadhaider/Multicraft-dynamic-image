3d78e5153c5405dcace59246a179a920caadf66a
Asad Haider <asad@asadhaider.co.uk>
2012-11-16 22:23:37 +0000

    Added servers sample config file.
    Signed-off-by: Asad Haider <asad@asadhaider.co.uk>

---
+++ config/servers.sample.php
@@ -0,0 +1,17 @@
+<?php
+$servers = array(
+									array(
+													'API_URL'		=>	'http://10.0.0.1/multicraft/api.php',
+													'API_USER'		=>	'admin',
+													'API_PASS'		=>	'APIPASS',
+													'SERVER_IDS'	=>	array( 1, 2, 3, 4 )
+												),
+									array(
+													'API_URL'		=>	'http://10.0.0.2/multicraft/api.php',
+													'API_USER'		=>	'admin',
+													'API_PASS'		=>	'APIPASS',
+													'SERVER_IDS'	=>	array( 1, 2, 3, 4 )
+												),
+
+								)
+?>
